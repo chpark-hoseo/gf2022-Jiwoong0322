56067f108128476a3b28ac07d8ec6e67d013788d
Jiwoong0322 <20191184@vision.hoseo.edu>
2022-11-15 21:05:17 +0900

    유일객체Game 실습
    유일객체Game 실습

--- src/Game.h
+++ src/Game.h
@@ -10,8 +10,15 @@ class Game
 {
 	std::vector<GameObject*> m_gameObjects; // 부모 클래스인 GameObject의 주소를 받아 멤버변수로 선언
 public:
-	Game() { }
-	~Game() { }
+	static Game* Instance() {
+		if (s_pInstance == 0)
+		{
+			s_pInstance = new Game();
+			return s_pInstance;
+		}
+		return s_pInstance;
+	}
+	SDL_Renderer* getRenderer() const { return m_pRenderer; }
 
 	bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
 	void render();
@@ -21,8 +28,13 @@ public:
 	void clean();
 
 private:
+	Game() { }
+	static Game* s_pInstance; // 정적 멤버변수
+
 	SDL_Window* m_pWindow;
 	SDL_Renderer* m_pRenderer;
 	int m_currentFrame;
 	bool m_bRunning = false;
 };
+
+typedef Game TheGame;
